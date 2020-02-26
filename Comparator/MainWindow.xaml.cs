52fae4ca3991c55699ea9eb987b42c63f31e8edf
Anton Sobolev <93sant@mail.ru>
2020-02-26 15:43:24 +0300

    получение данных с ячейки

--- Comparator/MainWindow.xaml.cs
+++ Comparator/MainWindow.xaml.cs
@@ -15,18 +15,25 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Office.Interop.Excel;
 using Excel = Microsoft.Office.Interop.Excel;
+using Comparator.Properties;
 
 namespace Comparator
 {
 	/// <summary>
 	/// Логика взаимодействия для MainWindow.xaml
 	/// </summary>
+	/// 
+	public delegate void TextChangedEventHandler(object sender, TextChangedEventArgs e);
+
 	public partial class MainWindow : System.Windows.Window
 	{
 		public MainWindow()
 		{
 			InitializeComponent();
 		}
+
+		public string Cell { get; private set; }
+		public Worksheet worksheet { get; private set; }
 
 		private void Open_Click(object sender, RoutedEventArgs e)
 		{
@@ -35,33 +42,41 @@ namespace Comparator
 				OpenFileDialog openFileDialog = new OpenFileDialog();
 				openFileDialog.Filter = "Excel files|*.xlsx|All files|*.*";
 				openFileDialog.DefaultExt = "*.xlsx";
-				openFileDialog.Title= "Choose your destiny";
+				openFileDialog.Title = "Choose your destiny";
 				string filename = "";
 				if (openFileDialog.ShowDialog() == true)
 				{
 					filename = openFileDialog.FileName;
-					textBox.Text = filename;
+					//textBox.Text = filename;
 				}
-
+				//textBox.Clear();
 				Excel.Application ExcelApp = new Excel.Application();
 				ExcelApp.Visible = true;
-				ExcelApp.WindowState = Excel.XlWindowState.xlMaximized;
-				Excel.
+				//ExcelApp.WindowState = XlWindowState.xlMaximized;
 
 				//ExcelApp.DisplayFullScreen = true;
 
-				Excel.Workbook workbook;
-				Excel.Worksheet worksheet;
+				Workbook workbook;
+				//Worksheet worksheet;
 				workbook = ExcelApp.Workbooks.Open(filename);
-				worksheet = workbook.ActiveSheet();
+				worksheet = workbook.Worksheets[1];
+				Excel.Range range = (Excel.Range)worksheet.Cells[1, 1];
+				//Excel.Range range = worksheet.
+				//textBox.Text = range.Value2;
+				//string Cell = "";
+				//Cell = textBox1.Text;
+				//textBox.Text = worksheet.Range[Cell].Value;
+
 				//ExcelApp.Application.DisplayFullScreen=true;
 
 			}
-			catch (Exception)
+			catch (Exception ex)
 			{
-
+				MessageBox.Show(ex.Message);
+				//Close();
 			}
-			
+
 		}
+
 	}
 }
